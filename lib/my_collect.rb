51a5d12c8f4aa0aaa7c809f4fd57cc50de34a316
Steven Perfors <stevecanbuild@gmail.com>
2020-01-26 22:56:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,9 @@
 students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
 def my_collect(array)
-  my_collect(students) do |name|
+  my_collect(array) do |name|
     name.split(" ").first 
   end
 end
+
+my_collect(students)
